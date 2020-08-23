999f24778e3c4c4fca9630c076eff37fba430636
naotosu <re13brb26l28@hotmail.com>
2020-08-24 07:19:27 +0900

    ブランチ変更

--- Sort.java
+++ Sort.java
@@ -1,9 +1,12 @@
 class Sort {
     public static void main(String[] args) {
         int[] heights = { 172, 149, 152, 191, 155};
+        System.out.println("昇順で並べ替えます");
         java.util.Arrays.sort(heights);
         for (int height :heights) {
             System.out.println(height);
         }
     }
 }
+
+//ブランチをmaster => working　切り替え
