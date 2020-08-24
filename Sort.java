9a783225d4d5ee3d1d24c8bf7849cc1b8527a97f
naotosu <re13brb26l28@hotmail.com>
2020-08-24 12:06:34 +0900

    再uptest

--- Sort.java
+++ Sort.java
@@ -2,6 +2,7 @@ class Sort {
     public static void main(String[] args) {
         int[] heights = { 172, 149, 152, 191, 155};
         System.out.println("昇順で並べ替えます");
+        System.out.println("確認用に二回目のUPファイルです");
         java.util.Arrays.sort(heights);
         for (int height :heights) {
             System.out.println(height);
@@ -9,4 +10,4 @@ class Sort {
     }
 }
 
-//ブランチをmaster => working　切り替え
+//ブランチをmaster => working　切り替え　2回目
